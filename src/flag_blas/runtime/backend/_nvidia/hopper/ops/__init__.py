3e34c4ce92908eec9adc1780284586506f6caf91
bin913 <842884726@qq.com>
2026-08-10 16:48:02 +0800

    fix code-format

--- src/flag_blas/runtime/backend/_nvidia/hopper/ops/__init__.py
+++ src/flag_blas/runtime/backend/_nvidia/hopper/ops/__init__.py
@@ -18,14 +18,14 @@ if triton.__version__ >= "3.5":
     from .bfgemm import bfgemm  # noqa: F401
     from .cgemm import cgemm  # noqa: F401
     from .dgemm import dgemm  # noqa: F401
-    from .hgemm import hgemm  # noqa: F401
-    from .sgemm import sgemm  # noqa: F401
-    from .zgemm import zgemm  # noqa: F401
     from .group_gemm import (  # noqa: F401
         group_bfgemm,
         group_hgemm,
         group_mm,
         group_tf32gemm,
     )
+    from .hgemm import hgemm  # noqa: F401
+    from .sgemm import sgemm  # noqa: F401
+    from .zgemm import zgemm  # noqa: F401
 
 __all__ = ["*"]
